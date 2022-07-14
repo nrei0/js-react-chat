28ae8fad2eafccce1760881f90b2a19041c01f71
root <root@NB-TO-5.vast.int>
2022-07-14 15:49:06 +0300

    added a strict background; fixed the message bar; added scrollbar; increased inputs, processed hovers

--- apiServer.js
+++ apiServer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const crypto = require("crypto");
 const bodyParser = require("body-parser");
 
-const port = 8001;
+const port = 8080;
 const app = express();
 
 const generateHash = () => {
